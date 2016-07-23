609d765df66865ba0016f6fc19b4582345ccd4a4
VivH <vivtsehoang@gmail.com>
2016-07-23 22:18:17 +0000

    Completed first part of flask assessment.

--- application.py
+++ application.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from flask_debugtoolbar import DebugToolbarExtension
+from flask import Flask, render_template, request
+#from flask_debugtoolbar import DebugToolbarExtension
 
 
 app = Flask(__name__)
@@ -8,16 +8,38 @@ app = Flask(__name__)
 # Required to use Flask sessions and the debug toolbar
 app.secret_key = "ABC"
 
+
 @app.route("/")
 def index_page():
     """Show an index page."""
 
-    return "<html><body>This is the homepage.</body></html>"
+    return render_template('index.html')
 
     # Alternately, we could make this a Jinja template in `templates/`
     # and return that result of rendering this, like:
     #
     # return render_template("index.html")
+
+
+@app.route("/application-form")
+def app_form():
+
+    return render_template('application-form.html')
+
+
+@app.route("/application", methods=['POST'])
+def app_confirmation():
+
+    first_name = request.form.get("firstname")
+    last_name = request.form.get("lastname")
+    salary = request.form.get("salary")
+    job = request.form.get("job")
+
+    return render_template('application-response.html',
+                            first_name=first_name,
+                            last_name=last_name,
+                            salary=salary,
+                            job=job)
 
 
 if __name__ == "__main__":
@@ -26,7 +48,6 @@ if __name__ == "__main__":
     app.debug = True
 
     # Use the DebugToolbar
-    DebugToolbarExtension(app)
+    #DebugToolbarExtension(app)
 
     app.run(host="0.0.0.0")
-
